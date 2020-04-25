201d1a5b9f502ea8dd0c0d2b4472fa7ab066e361
Ben Hedgepeth <airmaxmoney@protonmail.com>
2020-04-24 22:58:57 -0700

    Project 10 Flask API

--- todos/auth.py
+++ todos/auth.py
@@ -11,7 +11,6 @@ auth = MultiAuth(token_auth, basic_auth)
 @basic_auth.verify_password
 def verify_password(username, password):
     try:
-        import pdb; pdb.set_trace()
         api_user = User.get(User.username == username)
     except User.DoesNotExist:
         return False
